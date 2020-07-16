8529081447a06aad71d3412f496fc41a85c99fb6
pehcy <cheeyung.peh@gmail.com>
2020-07-16 18:18:32 +0800

    Updates

--- component---src-pages-404-js-9b32f3107659743c87fc.js
+++
@@ -1,2 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[5],{w2l6:function(t,e,n){"use strict";n.r(e);n("q1tI");var c=n("Bl7J"),u=n("vrFN"),o=n("qKvR");e.default=function(){return Object(o.c)(c.a,null,Object(o.c)(u.a,{title:"404: Not found"}),Object(o.c)("h1",null,"NOT FOUND"),Object(o.c)("p",null,"You just hit a route that doesn't exist... the sadness."))}}}]);
-//# sourceMappingURL=component---src-pages-404-js-9b32f3107659743c87fc.js.map
