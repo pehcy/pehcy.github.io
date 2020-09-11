3c66082709ba3bedfd0dbe80ce76eae5866fa8c0
pehcy <cheeyung.peh@gmail.com>
2020-09-11 22:49:09 +0800

    added shadowing for theme ui

---
+++ src/gatsby-plugin-theme-ui/index.js
@@ -0,0 +1,18 @@
+export default {
+    initialColorModeName: 'light',
+    useLocalStorage: false,
+    colors: {
+        text: "#333",
+        background: '#fff',
+        primary: '#639',
+        secondary: '#ff6347',
+        modes: {
+            dark: {
+                text: '#fff',
+                background: '#060606',
+                primary: '#3cf',
+            },
+        },
+    },
+}
+
