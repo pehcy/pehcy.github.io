ef6ae2559da2e0e1e24d6c04cc3e78fc1a947b2e
pehcy <cheeyung.peh@gmail.com>
2020-10-05 22:51:57 +0800

    Added back again SEO component

--- src/pages/articles.js
+++ src/pages/articles.js
@@ -2,16 +2,66 @@ import React from "react"
 import SEO from "../components/seo"
 import Layout from "../components/Layout"
 import Header from "../components/Header"
+import ArticleFeed from "../components/Articles"
+import { Container } from "../components/Container"
+import { graphql } from "gatsby"
+
+import { Global } from '@emotion/core'
+import { globalStyles } from '../utils/global-css' 
 
 const NotesPage = ({data}) => {
 
   return (
     <Layout>
+      <Global styles={ globalStyles } />
       <Header/>
       <SEO title="Notes" />
-      <h3>Self-learning Notes</h3>
+      <Container>
+        <h3>Articles</h3>
+      </Container>
+      <ArticleFeed edges={data.allMarkdownRemark.edges}/>
     </Layout>
   )
 }
 
 export default NotesPage
+
+export const pageQuery = graphql`
+  query {
+    site {
+      siteMetadata {
+        author {
+          name
+          summary
+          contacts {
+            twitter
+            linkedin
+            discord
+            rss
+          }
+        }
+        social {
+          twitter
+        }
+      }
+    }
+    allMarkdownRemark(
+      sort: { fields: [frontmatter___date], order: DESC },
+      filter: { frontmatter: { template: { eq: "post" }}}
+      ){
+      edges {
+        node {
+          excerpt
+          fields {
+            slug
+          }
+          frontmatter {
+            date(formatString: "MMMM DD, YYYY")
+            title
+            description
+          }
+        }
+      }
+    }
+  }
+`
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,29 +1,27 @@
 import React from "react"
-import { Link, graphql } from "gatsby"
+import { graphql } from "gatsby"
 
+import SEO from "../components/SEO"
 import IntroContext from "../components/Intro"
 import Layout from "../components/Layout"
 import Header from "../components/Header"
 import Footer from "../components/Footer"
 import SocialLinks from "../components/SocialLinks"
-import ArticleFeed from "../components/Articles"
 import RepositoriesList from "../components/Repo"
 
 import { Global } from '@emotion/core'
-import { globalStyles } from '../utils/global-css' 
-//import SEO from "../components/SEO"
-//import { rhythm } from "../utils/typography"
+import { globalStyles } from '../utils/global-css'
 
 const BlogIndex = ({ data, pageContext }) => {
-  //const siteTitle = data.site.siteMetadata.title
+  const siteTitle = data.site.siteMetadata.title
 
   return (
     <Layout>
+      <SEO title={ siteTitle } />
       <Global styles={ globalStyles } />
       <Header/>
       <IntroContext/>
-      <SocialLinks social={data.site.siteMetadata.social}/>
-      <ArticleFeed edges={data.allMarkdownRemark.edges}/>
+      <SocialLinks social={data.site.siteMetadata.author.contacts}/>
       <RepositoriesList/>
       <Footer/>
     </Layout>
@@ -36,15 +34,20 @@ export const pageQuery = graphql`
   query {
     site {
       siteMetadata {
+        title
+        description
         author {
           name
           summary
+          contacts {
+            twitter
+            linkedin
+            discord
+            rss
+          }
         }
         social {
           twitter
-          linkedin
-          discord
-          rss
         }
       }
     }
