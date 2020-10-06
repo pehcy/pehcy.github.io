4667be097638f2f4fb34f21c75ba5717650c291d
pehcy <cheeyung.peh@gmail.com>
2020-10-06 23:22:07 +0800

    feat: added dark mode toggle

---
+++ src/components/Header/toggle.tsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import { useColorMode } from 'theme-ui'
+
+export default function DarkModeToggle() {
+  const [colorMode, setColorMode] = useColorMode()
+  const isLight = colorMode === 'light'
+
+  const toggleTheme = (event : React.MouseEvent<HTMLElement>) => {
+    event.preventDefault()
+    setColorMode(isLight ? 'dark' : 'light')
+  }
+
+  return (
+    <>
+      <button
+        onClick = { toggleTheme }
+        aria-label={ isLight ? 'Light mode activated' : 'Dark mode activated'} 
+      >
+        Toggle
+      </button>
+    </>
+  )
+}
