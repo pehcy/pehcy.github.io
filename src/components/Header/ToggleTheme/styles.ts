655467d611b530606d7f1e9f5dabb30ea4e68041
pehcy <cheeyung.peh@gmail.com>
2020-10-08 00:53:52 +0800

    fix: unmatched daynight icon for header

--- src/components/Header/ToggleTheme/styles.ts
+++ src/components/Header/ToggleTheme/styles.ts
@@ -2,9 +2,13 @@ import styled from '@emotion/styled'
 
 // Day/Night Switch reference: https://codepen.io/aaroniker/pen/KGpXZo
 
+type themeProps = {
+  readonly isLight: boolean;
+  theme: any;
+}
 
 export const BtnWrapper = styled.button`
-  opacity: 1/2;
+  opacity: 0.65;
   border-radius: 5px;
   width: 40px;
   height: 25px;
@@ -12,24 +16,25 @@ export const BtnWrapper = styled.button`
   align-items: center;
   justify-content: center;
   transition: opacity 0.3s ease;
-  margin-left: 30px;
+  margin-left: 15px;
   background: rgba(255, 255, 255, 0.01);
   border: transparent;
   &:hover { opacity: 1; }
 `
 
-export const DayNight = styled.div<{isLight: boolean, theme: any}>`
+export const DayNight = styled.div<themeProps>`
   cursor: pointer;
   position: relative;
-  height: 25px;
-  width: 25px;
+  height: 24px;
+  width: 24px;
   border-radius: 50%;
-  border: ${p => (p.isLight ? "2px" : "4px")} solid ${p => p.theme.colors.primary};
-  transform: scale(${p => (p.isLight ? 1 : 0.55)});
+  background: ${p => p.theme.colors.primary};
+  border: ${p => (p.isLight ? "4px" : "2px")} solid ${p => p.theme.colors.primary};
+  transform: scale(${p => (p.isLight ? 0.57 : 1)});
   transition: all 0.45s ease;
-  overflow: ${p => (p.isLight ? "hidden" : "visible")};
+  overflow: ${p => (p.isLight ? "visible" : "hidden")};
 
-  &::before {
+  &::after {
     content: '';
     position: absolute;
     right: -9px;
@@ -38,11 +43,12 @@ export const DayNight = styled.div<{isLight: boolean, theme: any}>`
     width: 24px;
     border: 2px solid ${p => p.theme.colors.primary};
     border-radius: 50%;
-    transform: translate(${p => (p.isLight ? "0,0" : "14px, -14px" )});
-    opacity: ${p => (p.isLight ? 1 : 0 )};
+    transform: translate(${p => (p.isLight ?  "0, 0" : "14px, -14px")});
+    transition: transform 0.45s ease;
+    opacity: ${p => (p.isLight ? 0 : 1 )};
   }
 
-  &::after {
+  &::before {
     content: '';
     width: 8px;
     height: 8px;
@@ -59,12 +65,12 @@ export const DayNight = styled.div<{isLight: boolean, theme: any}>`
                 -15px 15px 0 ${p => p.theme.colors.primary},
                 15px -15px 0 ${p => p.theme.colors.primary},
                 -15px -15px 0 ${p => p.theme.colors.primary};
-    transform: scale(${p => (p.isLight ? 0 : 1)});
+    transform: scale(${p => (p.isLight ? 1 : 0)});
     transition: all 0.35s ease;
   }
 `
 
-export const IconLayer = styled.div<{isLight: boolean, theme: any}>`
+export const IconLayer = styled.div<themeProps>`
   position: absolute;
   right: -1px;
   top: -8px;
@@ -72,5 +78,8 @@ export const IconLayer = styled.div<{isLight: boolean, theme: any}>`
   width: 24px;
   border-radius: 50%;
   border: 0;
+  background: ${p => p.theme.colors.background};
+  transform: translate(${p => (p.isLight ? "14px, -14px" : "0, 0")});
   transition: ${p => p.theme.colors.primary}, transform 0.45s ease;
+  opacity: ${p => (p.isLight ? 0 : 1)};
 `
