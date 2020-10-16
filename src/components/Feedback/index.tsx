5c09d58f8b67d9527faf57baef84f12bbf735257
pehcy <cheeyung.peh@gmail.com>
2020-10-16 17:43:54 +0800

    refactor: refactor the main logics

--- src/components/Feedback/index.tsx
+++ src/components/Feedback/index.tsx
@@ -1,28 +1,82 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
+import { Container } from '../Container'
 import axios from 'axios'
 
-const FeedbackForm = () => {
-
-  const [robot, isRobot] = useState<boolean>(false)
-
-  isRobot(true)
-
-  function updateSubmit(event: React.FormEvent<HTMLElement>) {
-    event.preventDefault()
-  }
-
-  return(
-    <form action="https://formspree.io/f/{form_id}" acceptCharset="UTF-8" method="post">
-      <fieldset>
-        <label>Full Name</label>
-        <input type="email" name="_replyto" id="_replyto"/>
-        <textarea placeholder="Leave your message here...">
-        </textarea>
-        <input type="hidden" name="_gotcha" id="_gotcha" style={{display: 'none'}} />
-      </fieldset>
-      <button onClick={ updateSubmit }></button>
-    </form>
-  )
+interface feedbackProps {
+  fullname: string;
+  email: string;
+  message: string;
+  hidden?: string;
 }
 
-export default FeedbackForm
+export default function FeedbackForm() {
+  const [isRobot, setIsRobot] = useState<boolean>(false)
+  const [inputs, setInputs] = useState<feedbackProps>({
+    fullname: '',
+    email: '',
+    message: '',
+    hidden: '',
+  })
+
+  function handleOnChange(event: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) {
+    event.persist()
+    setInputs(prev => ({
+      ...prev,
+      [event.target.id]: event.target.value,
+    }))
+    setIsRobot(!setInputs['hidden'].value.length)
+  }
+
+  useEffect(() => {
+    if (Object.keys()){
+
+    }
+  }, [inputs])
+
+  async function handleOnSubmit(event: React.FormEvent<HTMLElement>){
+    event.preventDefault()
+    if(!setIsRobot) {
+      try{
+        await axios({
+          method: "POST",
+          url: "https://formspree.io/YOUR_FORM_ID",
+          data: JSON.stringify(inputs),
+        })
+        .then()
+        setTimeout(() => resetForm(), 4000)
+      }
+      catch(e){
+        console.warn(e)
+      }
+    }
+  }
+
+  const resetForm = () => {
+    Object.keys(setInputs).forEach(attr => setInputs[attr] = '')
+  }
+
+  return (
+    <Container>
+      <form onSubmit={ handleOnSubmit } acceptCharset='UTF-8' noValidate>
+        <fieldset>
+          <label htmlFor="fullname">Full Name</label>
+          <input
+            id="fullname"
+            type="text"
+          />
+          <label htmlFor="email">Email</label>
+          <input
+            id="email"
+            name="email"
+          />
+          <label htmlFor="message">Message</label>
+          <textarea
+            placeholder="Leave your message here..."
+            value = { inputs.message }
+            onChange={ handleOnChange }
+          />
+        </fieldset>
+      </form>
+    </Container>
+  )
+}
