a825eecfe523dba9cfee95261566b38df5319af9
pehcy <cheeyung.peh@gmail.com>
2020-10-31 23:49:51 +0800

    refactor: simplify form with formspree

--- src/components/Feedback/index.tsx
+++ src/components/Feedback/index.tsx
@@ -1,111 +1,50 @@
-import React, { useState, useEffect } from 'react'
+import React from 'react'
+//import { Formik, Form, Field, ErrorMessage } from 'formik'
+//import * as Yup from 'yup'
 import { Container } from '../Container'
 import { author } from '../../gatsby/data/config'
 import axios from 'axios'
 
-interface feedbackProps {
-  fullname: string;
-  email:    string;
-  message:  string;
-  _gotcha:  string;
-}
+export default class FeedbackForm extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      fullname: '',
+      email: '',
+      message: '',
+    }
+    this.handleOnChange = this.handleOnChange.bind(this)
+  }
 
-export default function FeedbackForm() {
-  const [isRobot, setIsRobot] = useState<boolean>(false)
-  const [fieldError, setFieldError] = useState<object>({})
-
-  const [inputs, setInputs] = useState<feedbackProps>({
-    fullname: '',
-    email: '',
-    message: '',
-    _gotcha: '',
-  })
-
-  function handleOnChange(event: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) {
+  handleOnChange(event: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) {
     event.persist()
-    setInputs(prev => ({
-      ...prev,
-      [event.target.id]: event.target.value,
-    }))
-    //setIsRobot(!setInputs['hidden'].value.length)
+    this.setState({[event.target.id]: event.target.value })
   }
 
-  const validateScheme = {
-    fullname: val => !!val,
-    email: val => /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(val),
-    message: msg => !!msg,
-    _gotcha: val => !val
-  }
-
-  const isValid = () => {
-    const errors = Object.keys(inputs).filter(attr => !validateScheme[attr](inputs[attr]))
-    setFieldError(prev => ({ ...prev, ...errors}))
-    return errors.hasOwnProperty('key')
-  }
-
-  useEffect(() => {
-    if (Object.keys(fieldError).length > 0){
-      isValid()
-    }
-  }, [inputs])
-
-  async function handleOnSubmit(event: React.FormEvent<HTMLElement>){
+  async handleSubmit(event: React.FormEvent<HTMLElement>) {
     event.preventDefault()
-    if(isValid() || !isRobot) {
-      try{
-        await axios({
-          method: "POST",
-          url: `https://formspree.io/${author.contacts.email}`,
-          data: JSON.stringify(inputs),
-        })
-        .then(res => console.log(res))
-        setTimeout(() => resetForm(), 4000)
-      }
-      catch(e){
-        console.warn(e)
-      }
+    try {
+      await axios({})
+    } catch (err) {
+      alert(err)
     }
   }
 
-  function resetForm() {
-    Object.keys(setInputs).forEach(attr => setInputs[attr] = '')
-  }
-
-  return (
-    <Container>
-      <form onSubmit={ handleOnSubmit } acceptCharset='UTF-8' noValidate>
-        <fieldset>
-          <label htmlFor="fullname">Full Name</label>
-          <input
-            id="fullname"
-            type="text"
-            value = { inputs.fullname }
-            onChange={ handleOnChange }
-          />
-          <label htmlFor="_replyto">Email</label>
-          <input
-            id="email"
-            name="_replyto"
-            value = { inputs.email }
-            onChange={ handleOnChange }
-          />
+  render() {
+    return (
+      <Container>
+        <form action={`https://formspree.io/${process.env.GATSBY_FORMSPREE_ENDPOINT}`} method="POST">
+          <input type="text" name="_gotcha" style={{ display: `none`}} />
+          <label htmlFor="name">Full Name</label>
+          <input type="text" name="name" id="fullname" />
+          <label htmlFor="email">Email</label>
+          <input type="email" name="_replyto" id="email" />
           <label htmlFor="message">Message</label>
-          <textarea
-            name="message"
-            id="message"
-            placeholder="Leave your message here..."
-            value = { inputs.message }
-            onChange={ handleOnChange }
-          />
-          <input type="hidden" id="_gotcha" name="_gotcha" 
-            value={ inputs._gotcha }
-            onChange={ handleOnChange } 
-          />
-          <button type="submit">
-            Submit
-          </button>
-        </fieldset>
-      </form>
-    </Container>
-  )
+          <textarea name="message" id="message" 
+            placeholder="Please leave your message here..."/>
+          <button type="submit">Submit</button>
+        </form>
+      </Container>
+    )
+  }
 }
