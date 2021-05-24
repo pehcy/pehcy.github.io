019fb8a42b88974f2e9257de02fb95bb736f04c7
pehcy <cheeyung.peh@gmail.com>
2021-05-24 22:32:07 +0800

    homepage template

---
+++ src/components/Layout/index.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Layout = ({ children }) => (
+  <div className="mx-auto flex flex-col">
+    { children }
+  </div>
+)
+
+export default Layout
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,19 +1,27 @@
 import React from 'react'
 import { Link, graphql } from 'gatsby'
-import Layout from '../components/layout'
+import Layout from '../components/Layout'
+import Header from '../components/Header'
 
 const IndexPage = ({ data }) => (
   <Layout>
-    <div>
-      {
-        data.allMdx.nodes.map(({excerpt, frontmatter, fields}) => (
-          <Link to={fields.slug}>
-            <h1>{ frontmatter.title }</h1>
-            <p>{ frontmatter.date }</p>
-            <p>{ excerpt }</p>
-          </Link>
-        ))
-      }
+    <Header />
+    <div className="m-auto md:max-w-5xl py-6">
+      <h1>Recent Posts</h1>
+      <div>
+        {
+          data.allMdx.nodes.map(({ excerpt, frontmatter, fields }) => (
+            <Link to={fields.slug}>
+              <h2>{frontmatter.title}</h2>
+              <div className="grid grid-cols-2 gap-1 box-border md:w-9/12">
+                <div className="text-sm font-sans">Last updated on {new Date(frontmatter.date).toLocaleString('en-GB')}</div>
+                <div className="text-sm font-sans">5 min read</div>
+              </div>
+              <p>{excerpt}</p>
+            </Link>
+          ))
+        }
+      </div>
     </div>
   </Layout>
 )
