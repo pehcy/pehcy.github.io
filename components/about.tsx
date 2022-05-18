1776d5f24809790358cb386a0074a4a4144c84da
pehcy <cheeyung.peh@gmail.com>
2022-05-19 00:27:30 +0800

    Feat: added resume page section

--- components/about.tsx
+++ components/about.tsx
@@ -1,14 +1,14 @@
 import React from 'react';
 
 const About = () : JSX.Element => (
-  <>
+  <div className="grow border-l h-full pl-20">
     <div className="flex flex-row py-8">
       <h2 className="basis-1/4 font-bold text-2xl my-auto px-8">Now</h2>
       <div className="basis-3/4 flex flex-col max-w-2xl">
         <h2 className="text-2xl font-semibold mb-1">Infovista</h2>
         <div className="mb-4">Massy, France (Fulltime, Remote)</div>
-        <div className="text-gray-500">I'm now working as a software engineer in Infovista, 
-          I recently deployed several projects 
+        <div className="text-gray-500">I'm now working as a software engineer 
+          in Infovista, I recently deployed several projects 
           including IPerf3, OTT Files Sharing Libraries on Azure DevOps.
           Integrate the existing testing tools to Ubuntu, Redhat Linux.
         </div>
@@ -20,13 +20,13 @@ const About = () : JSX.Element => (
         <h2 className="text-2xl font-semibold mb-1">Appxplore (iCandy)</h2>
         <div className="mb-4">Kuala Lumpur, Malaysia (Fulltime)</div>
         <div className="text-gray-500">Appxplore is a 
-        mobile game studio in Kuala Lumpur.  I have been worked as 
-        a full stack NodeJS engineer to help to extend the 
-        ClawStars game support.
+          mobile game studio in Kuala Lumpur.  I have been worked as 
+          a full stack NodeJS engineer to help to extend the 
+          ClawStars game support.
         </div>
       </div>
     </div>
-  </>
+  </div>
 );
 
 export default About;
--- pages/index.tsx
+++ pages/index.tsx
@@ -10,6 +10,15 @@ const Home : NextPage = () => {
       <Header />
       <div className="mx-auto md:max-w-5xl">
         <Hero />
+      </div>
+      <div className="mx-auto md:max-w-full flex">
+        <nav className="w-auto h-full sticky top-[15.5rem]">
+          <ul className="overflow-hidden flex flex-col items-center">
+            <span className="pb-2">Experience</span>
+            <span className="pb-2">Education</span>
+            <span className="pb-2">Skills</span>
+          </ul>
+        </nav>
         <About />
       </div>
     </Layout>
